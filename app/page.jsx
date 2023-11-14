940b6be000e7d0b274e2ddcecd8e9137369d8865
isaiahaquino <isaiahaquino1997@gmail.com>
2023-11-13 18:45:23 -0800

    home path routing

--- app/page.jsx
+++ app/page.jsx
@@ -2,20 +2,23 @@
 
 import { useSession } from "next-auth/react"
 import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Home() {
   const { data } = useSession()
   const router = useRouter()
 
-  if (!data) {
-    router.push("/auth")
-  } else {
-    router.push("/dashboard")
-  }
+  useEffect(() => {
+    if (!data) {
+      router.push("/auth")
+    } else {
+      router.push("/dashboard")
+    }
+  }, [data])
 
   return (
     <main>
-      <h1>home</h1>
+
     </main>
   )
 }
